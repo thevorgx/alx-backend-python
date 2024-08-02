baf56bb9e5b51917e71b57498d57a09dc9060353
thevorgx <kh.elallali@gmail.com>
2024-08-02 01:46:21 +0100

    update

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """concat two strings"""
 
 
 def concat(str1: str, str2: str) -> str:
-    """concat two strings"""
+    """concat 2 strings"""
     return (str1 + str2)
