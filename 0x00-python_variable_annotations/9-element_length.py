ff6ca6ab5274cfb72c42f1050afdc53cee0a9bc0
thevorgx <kh.elallali@gmail.com>
2024-08-02 03:08:10 +0100

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Let's duck type an iterable object"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """element len with the correct annotations"""
+    return [(i, len(i)) for i in lst]
