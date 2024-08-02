1c8833330a20a5bfb946e7f602856888aaf2e556
thevorgx <kh.elallali@gmail.com>
2024-08-02 02:33:48 +0100

    to kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Complex types - string and int/float to tuple"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """tuple with a string and the square of a number"""
+    square_v: float = v ** 2
+    tup: Tuple[str, float] = (k, square_v)
+    return (tup)
