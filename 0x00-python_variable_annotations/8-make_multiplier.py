b121c9d05f53c6ebbec4b0f48ef2a80bac8beb04
thevorgx <kh.elallali@gmail.com>
2024-08-02 02:51:48 +0100

    update

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""return a function that multiply a float val by multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """function that multiply a float val by multiplier"""
+    def vorg_multiplier_dynamo(v: float) -> float:
+        """function to return result"""
+        res = v * multiplier
+        return (res)
+    return(vorg_multiplier_dynamo)
