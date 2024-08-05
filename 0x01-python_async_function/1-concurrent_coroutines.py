111dc9aea64bb4036faef8fe6890042acb5df0bc
thevorgx <kh.elallali@gmail.com>
2024-08-05 11:28:47 +0100

    sort in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for value in range(n):
         value = await wait_random(max_delay)
         listo.append(value)
-    return (listo)
+    new_listo = sorted(listo)
+    return (new_listo)
