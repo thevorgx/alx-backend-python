2c77a2bbca103b3894421c50d95e4bccbe521c50
thevorgx <kh.elallali@gmail.com>
2024-08-06 11:49:41 +0100

    annotation update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,12 @@
 """Async Generator"""
 import asyncio
 from random import uniform
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """yield a number each iteration"""
     for _ in range(10):
-        await asyncio.sleep(0)
+        await asyncio.sleep(1)
         rand_number = uniform(0, 10)
         yield (rand_number)
