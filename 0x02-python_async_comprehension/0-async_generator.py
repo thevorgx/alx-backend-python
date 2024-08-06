358d1f0355ed3cfbb74551f032df958a7724d285
thevorgx <kh.elallali@gmail.com>
2024-08-06 12:07:37 +0100

    update wth ?

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,8 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         rand_number = random.uniform(0, 10)
-        if rand_number == 0:
-            rand_number += 0.01
-        elif rand_number == 10:
-            rand_number -= 0.01
         yield (rand_number)
