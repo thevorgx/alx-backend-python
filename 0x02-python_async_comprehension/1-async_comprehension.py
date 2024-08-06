a63f364f23837c4dd11a4d2cf8935a7cb7b44421
thevorgx <kh.elallali@gmail.com>
2024-08-06 12:30:45 +0100

    use async comprehensions instead of appending a list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """coroutine that collect random numbers for async_gen"""
-    listo = []
-    async for i in async_generator():
-        listo.append(i)
-    return (listo)
+    return [rand_number async for rand_number in async_generator()]
