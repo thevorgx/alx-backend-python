fb923945b374d73a459020897882cdec5d341ee5
thevorgx <kh.elallali@gmail.com>
2024-08-26 20:53:53 +0100

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """test_utils module"""
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
 from utils import access_nested_map as anp
+from utils import get_json as gj
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,3 +29,22 @@ class TestAccessNestedMap(unittest.TestCase):
         for KeyError Raises"""
         with self.assertRaises(KeyError):
             anp(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """place holder for requirements"""
+    @parameterized.expand([
+        ('http://example.com', {"payload": True}),
+        ('http://holberton.io', {"payload": False})
+    ])
+    @patch('utils.get_json')
+    def test_get_json(self, test_url, expected_payload, mock_get_json):
+        "place holder for the other requirements"
+        if test_url == 'http://example.com':
+            mock_get_json.return_value = {"payload": True}
+        elif test_url == 'http://holberton.io':
+            mock_get_json.return_value = {"payload": False}
+
+        res = gj(test_url)
+        self.assertEqual(res, expected_payload)
+        mock_get_json.assert_called_once_with(test_url)
