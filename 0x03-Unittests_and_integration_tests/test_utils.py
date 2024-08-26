63c9f141644a84e5242300bd2844a00c468d6311
thevorgx <kh.elallali@gmail.com>
2024-08-27 00:17:23 +0100

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map as anp
 from utils import get_json as gj
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -39,7 +40,7 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch('utils.requests.get')
     def test_get_json(self, test_url, expected_payload, mock_get_json):
-        "test get_json method as 'gj'"
+        "test get_json method from utils.py as 'gj'"
         mock_response = Mock()
         mock_response.json.return_value = expected_payload
         mock_get_json.return_value = mock_response
@@ -47,3 +48,30 @@ class TestGetJson(unittest.TestCase):
         res = gj(test_url)
         self.assertEqual(res, expected_payload)
         mock_get_json.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """test class: TestMemoize"""
+    def test_memoize(self):
+        """test memoize method from utils.py"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
+            res1 = test_instance.a_property
+            res2 = test_instance.a_property
+            """for future vorg: Calling a_property x2
+            checks that it remembers the result from the first call,
+            so a_method runs only once"""
+        self.assertEqual(res1, 42)
+        self.assertEqual(res2, 42)
+
+        mock_method.assert_called_once()
